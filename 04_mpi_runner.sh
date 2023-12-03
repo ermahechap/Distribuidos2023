e6f2b8c5929f8714316c5d1ec4b380e0363a4d38
ermahechap <ermahechap@unal.edu.co>
2023-12-02 22:26:04 -0500

    Add runner code

---
+++ 04_mpi_runner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+n_iters=5
+audio_durations=("01" "05" "10" "20" "30" "40")
+msg_lengths=("100" "1000" "10000" "100000" "1000000")
+number_of_ranks=(1 2 4 8)
+
+for duration in "${audio_durations[@]}"; do
+    for len in "${msg_lengths[@]}"; do
+        for n_ranks in "${number_of_ranks[@]}"; do
+            for ((i=1; i<=n_iters; i++)); do
+                echo -n "duration: ${duration}, msg_len: ${len}, n_ranks: ${n_ranks}, iteration: ${i}, " &&
+                mpirun -np ${n_ranks} "04 MPI/encode.o" -i "./Samples/custom_testcases/${duration}.wav" -o "./Outputs/Audio/c_out.wav" -m "./MessageSamples/${len}.txt" -t
+                sleep 1
+            done
+        done
+    done
+done
